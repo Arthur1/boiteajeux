fb9fd9b058a3dcaa4b5b883eaf0964c07f40b457
Kazuki Asakura <arthur.1121lemsche@gmail.com>
2019-12-25 08:39:33 +0900

    init

---
+++ app/src/main/java/buratsuki/arthur1/boiteajeux/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package buratsuki.arthur1.boiteajeux.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState (val usernameError: Int? = null,
+                      val passwordError: Int? = null,
+                      val isDataValid: Boolean = false)
---
+++ app/src/main/java/buratsuki/arthur1/boiteajeux/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package buratsuki.arthur1.boiteajeux.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult (
+     val success:LoggedInUserView? = null,
+     val error:Int? = null
+)
